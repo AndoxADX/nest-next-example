ef285a139f0f6c406dcf1d5ba87a8ce2da986786
Andox <andoxyap@outlook.com>
2022-05-28 23:50:38 +0800

    partial till before Wrapper for fetch

---
+++ src/server/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Render } from '@nestjs/common';
+import { map, toArray } from 'rxjs';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // @Render('index')
+  // home() {
+  //   return {};
+  // }
+
+  @Get('/')
+  @Render('index')
+  home() {
+    return this.appService.getBlogPosts().pipe(
+      toArray(),
+      map((blogPosts) => ({ blogPosts })),
+    );
+  }
+
+  @Get(':id')
+  @Render('[id]')
+  public blogPost(@Param('id') id: string) {
+    return {};
+  }
+}
